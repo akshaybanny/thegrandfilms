bc76e967f5b5d15c0cd5c1a533d0b6b25e20f2d0
Akshay <Akshay@DESKTOP-EQU5D95>
2022-07-20 22:52:48 +0530

    [raja24]:filmywapdata1

---
+++ src/rajathegreat/sample2.java
@@ -0,0 +1,14 @@
+package rajathegreat;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder atb = new StringBuilder("car"); 
+		System.out.println(atb.reverse());
+		
+		StringBuffer ner=new StringBuffer("yahska");
+		System.out.println(ner.replace(0, 6, "yash"));
+	}
+
+}
